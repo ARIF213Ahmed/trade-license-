2deb64f804608d49c92581454e35c705c3616cbc
Arif Ahmed <arifahmed30340@gmail.com>
2025-05-11 11:49:19 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,154 +1,34 @@
 const tradeData = [
-  {
-    name: "ADVENTURE TOURISM & TREKKING OPERATOR",
-    type: "GTL",
-    code: "902"
-  },
-  {
-    name: "ADVERTISING, IT SERVICES & PUBLICATION",
-    type: "GTL",
-    code: "942"
-  },
-  {
-    name: "AGRICULTURAL BROKERAGE & LIVESTOCK SUPPLY",
-    type: "GTL",
-    code: "999"
-  },
-  {
-    name: "AGRICULTURAL CHEMICALS & FERTILIZERS",
-    type: "GTL",
-    code: "998"
-  },
-  {
-    name: "AGRICULTURE & FARMING PRODUCT",
-    type: "GTL",
-    code: "997"
-  },
-  {
-    name: "ALMIRAH MFG",
-    type: "ITL",
-    code: "9806"
-  },
-  {
-    name: "ALUMINIUM DOOR & WINDOW",
-    type: "ITL",
-    code: "9797"
-  },
-  {
-    name: "ALUMINIUM FABRICATION",
-    type: "ITL",
-    code: "9796"
-  },
-  {
-    name: "ANIMAL NUTRITION & SUPPLEMENTS",
-    type: "GTL",
-    code: "996"
-  },
-  {
-    name: "ARCHITECTURE & DESIGN SERVICES",
-    type: "GTL",
-    code: "995"
-  },
-  {
-    name: "ART & CRAFT",
-    type: "GTL",
-    code: "889"
-  },
-  {
-    name: "ASTROLOGY & SPIRITUAL CONSULTANCY",
-    type: "GTL",
-    code: "884"
-  },
-  {
-    name: "ATTA CHAKKI",
-    type: "ITL",
-    code: "9682"
-  },
-  {
-    name: "ATTA PACKAGING AND FLOUR MILL",
-    type: "ITL",
-    code: "9679"
-  },
-  {
-    name: "AUTO & BIKE PARTS",
-    type: "GTL",
-    code: "888"
-  },
-    {
-    name: "AUTOMOTIVE SALES & DEALERSHIP",
-    type: "GTL",
-    code: "994"
-  },
-  {
-    name: "AYURVEDIC MEDICINE MFG",
-    type: "ITL",
-    code: "9590"
-  },
-  {
-    name: "BAG MATERIAL MFG AND SALE",
-    type: "ITL",
-    code: "9569"
-  },
-  {
-    name: "BAKERY & CONFECTIONERY",
-    type: "GTL",
-    code: "993"
-  },
-  {
-    name: "BAKERY ITEM SELLING COUNTER (AIR CONDITIONED ESTABLISHMENT)",
-    type: "HTL",
-    code: "9548"
-  },
-  {
-    name: "BAKERY ITEM SELLING COUNTER (NON AIR CONDITIONED ESTABLISHMENT)",
-    type: "HTL",
-    code: "9547"
-  },
-  {
-    name: "BANKING & FINANCIAL SERVICES",
-    type: "GTL",
-    code: "992"
-  },
-  {
-    name: "BANQUET HALL HAVING 101 TO 150 SEATS CAPACITIES AT A TIME",
-    type: "HTL",
-    code: "9526"
-  },
-  {
-    name: "BANQUET HALL HAVING 151 SEATS AND ABOVE AT A TIME",
-    type: "HTL",
-    code: "9525"
-  },
-  {
-    name: "BANQUET HALL HAVING 51 TO 100 SEATS CAPACITIES AT A TIME",
-    type: "HTL",
-    code: "9524"
-  },
-  {
-    name: "BANQUET HALL HAVING UPTO 50 SEATS CAPACITIES AT A TIME",
-    type: "HTL",
-    code: "9523"
-  },
-  {
-    name: "BAR BENDED WIRE",
-    type: "ITL",
-    code: "9520"
-  },
-  {
-    name: "BATTERIES & AUTO COMPONENTS",
-    type: "GTL",
-    code: "890"
-  },
-  {
-    name: "BATTERIES & AUTO COMPONENTS",
-    type: "GTL",
-    code: "968"
-  },
-  {
-    name: "BEAUTY PARLOUR OR SALON (LADIES AND KIDS) WITH SPA OR OTHER SERVICES",
-    type: "HTL",
-    code: "9486"
-  },
+  { name: "ADVENTURE TOURISM & TREKKING OPERATOR", type: "GTL", code: "902" },
+  { name: "ADVERTISING, IT SERVICES & PUBLICATION", type: "GTL", code: "942" },
+  { name: "AGRICULTURAL BROKERAGE & LIVESTOCK SUPPLY", type: "GTL", code: "999" },
+  { name: "AGRICULTURAL CHEMICALS & FERTILIZERS", type: "GTL", code: "998" },
+  { name: "AGRICULTURE & FARMING PRODUCT", type: "GTL", code: "997" },
+  { name: "ALMIRAH MFG", type: "ITL", code: "9806" },
+  { name: "ALUMINIUM DOOR & WINDOW", type: "ITL", code: "9797" },
+  { name: "ALUMINIUM FABRICATION", type: "ITL", code: "9796" },
+  { name: "ANIMAL NUTRITION & SUPPLEMENTS", type: "GTL", code: "996" },
+  { name: "ARCHITECTURE & DESIGN SERVICES", type: "GTL", code: "995" },
+  { name: "ART & CRAFT", type: "GTL", code: "889" },
+  { name: "ASTROLOGY & SPIRITUAL CONSULTANCY", type: "GTL", code: "884" },
+  { name: "ATTA CHAKKI", type: "ITL", code: "9682" },
+  { name: "ATTA PACKAGING AND FLOUR MILL", type: "ITL", code: "9679" },
+  { name: "AUTO & BIKE PARTS", type: "GTL", code: "888" },
+  { name: "AUTOMOTIVE SALES & DEALERSHIP", type: "GTL", code: "994" },
+  { name: "AYURVEDIC MEDICINE MFG", type: "ITL", code: "9590" },
+  { name: "BAG MATERIAL MFG AND SALE", type: "ITL", code: "9569" },
+  { name: "BAKERY & CONFECTIONERY", type: "GTL", code: "993" },
+  { name: "BAKERY ITEM SELLING COUNTER (AIR CONDITIONED ESTABLISHMENT)", type: "HTL", code: "9548" },
+  { name: "BAKERY ITEM SELLING COUNTER (NON AIR CONDITIONED ESTABLISHMENT)", type: "HTL", code: "9547" },
+  { name: "BANKING & FINANCIAL SERVICES", type: "GTL", code: "992" },
+  { name: "BANQUET HALL HAVING 101 TO 150 SEATS CAPACITIES AT A TIME", type: "HTL", code: "9526" },
+  { name: "BANQUET HALL HAVING 151 SEATS AND ABOVE AT A TIME", type: "HTL", code: "9525" },
+  { name: "BANQUET HALL HAVING 51 TO 100 SEATS CAPACITIES AT A TIME", type: "HTL", code: "9524" },
+  { name: "BANQUET HALL HAVING UPTO 50 SEATS CAPACITIES AT A TIME", type: "HTL", code: "9523" },
+  { name: "BAR BENDED WIRE", type: "ITL", code: "9520" },
+  { name: "BATTERIES & AUTO COMPONENTS", type: "GTL", code: "890" },
+  { name: "BATTERIES & AUTO COMPONENTS", type: "GTL", code: "968" },
+  { name: "BEAUTY PARLOUR OR SALON (LADIES AND KIDS) WITH SPA OR OTHER SERVICES", type: "HTL", code: "9486" },
   { name: "BEAUTY PARLOUR OR SALON (LADIES AND KIDS) WITHOUT SPA OR OTHER SERVICES", type: "HTL", code: "9485" },
   { name: "BHOJANALAYA HAVING 16 SEATS TO 30 SEATS (AIR CONDITIONED ESTABLISHMENT)", type: "HTL", code: "9459" },
   { name: "BHOJANALAYA HAVING 16 SEATS TO 30 SEATS (NON AIR CONDITIONED ESTABLISHMENT)", type: "HTL", code: "9458" },
@@ -163,5 +43,6 @@ const tradeData = [
   { name: "BICYLE & FITNESS EQUIPMENT", type: "GTL", code: "886" },
   { name: "BINDING PRESS", type: "ITL", code: "9423" },
   { name: "BIO PESTICIDES", type: "GTL", code: "991" },
-  { name: "BLOOD BANK", type: "HTL", code: "9395" }
+  { name: "BLOOD BANK", type: "HTL", code: "9395" },
+  
 ];
